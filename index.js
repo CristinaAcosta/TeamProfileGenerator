a53edcd9e2af72d5446fd015500c364a90e1bccd
Cristina Acosta <cacost5695@yahoo.com>
2021-03-03 22:27:40 -0800

    Jest

---
+++ index.js
@@ -0,0 +1,14 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const managerQuestions = [{
+    message: 'What is your name?',
+      name: 'name',
+      type: 'input',
+},
+
+]
+
+nq.prompt(quest).then( ans => {
+    // Put HTML file 
+}
