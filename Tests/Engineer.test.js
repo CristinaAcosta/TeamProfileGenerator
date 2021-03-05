6309a6ccde2fcbebec25838e59079b2d806b1e40
Cristina Acosta <cacost5695@yahoo.com>
2021-03-04 21:49:26 -0800

    team

---
+++ Tests/Engineer.test.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const engineerQuestions = [
+{
+    message: 'What is their name?',
+      name: 'name',
+      type: 'input',
+},
+{
+    message: 'What is their ID number?',
+      name: 'id',
+      type: 'input',
+},
+{
+    message: 'What is their email?',
+      name: 'email',
+      type: 'input',
+},
+{
+    message: 'What is their Github username?',
+      name: 'github',
+      type: 'input',
+},
+]
+
+nq.prompt(quest).then( ans => {
+    // Put HTML file 
+}
