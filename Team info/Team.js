15287d2843f66bff97c9d464f10b3fe41db9c2bf
Cristina Acosta <cacost5695@yahoo.com>
2021-03-08 14:13:47 -0800

    Teamsssss'

--- Team info/Team.js
+++ Team info/Team.js
@@ -1,21 +1,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const manager = require ('./Manager')
+const manager = require ('./Manager');
+const Engineer = require ('./Engineer');
+const Intern = require('./Intern');
 
 class Team {
     constructor() {
         this.members = []
         inquirer.prompt(managerQuestions)
         .then ((data) => {
-            const manager = new manager(data.name, data.id, data.email, data.office);
-            this.members.push(managers)
-            this.more(data.add)
+            const manager = new Manager(data.name, data.id, data.email, data.office);
+            this.members.push(managers);
+            this.more(data.add);
         });
     }
-}
 
 printStats() {
-    console.log(`manager: ${this.members}`);
+    console.log(`manager:${this.members}`);
+
 }
 
-  
+
+add(role) {
+    if (role == "engineer") {
+        inquirer.prompt(engineerQuestions)
+        .then ((data) => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.GitHub);
+            this.members.push(engineer);
+            this.more(data.add);
+    });
+        else if(role == "intern") {
+            inquirer.prompt(internQuestions)
+            .then ((data) => {
+                const intern = new Intern(data.name, data.id, data.email, data.GitHub);
+                this.members.push(intern);
+                this.more(data.add);
+        });
+        }
